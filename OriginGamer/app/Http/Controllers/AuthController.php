51a4abc7d2510e4760885d288d2145366ff93f45
nouhaila el housni <112879456+spirithuntt@users.noreply.github.com>
2023-03-12 00:14:11 +0100

    created resetpassword

--- OriginGamer/app/Http/Controllers/AuthController.php
+++ OriginGamer/app/Http/Controllers/AuthController.php
@@ -104,5 +104,28 @@ class AuthController extends Controller
             : response()->json(['error' => 'Failed to send reset link'], 500);
     }
 
+    public function resetpassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $response = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ]);
+
+                $user->save();
+            }
+        );
+        // constant defined in Laravel's PasswordBroker class
+        // if smtg wrong it will return Password::INVALID_TOKEN
+        return $response == Password::PASSWORD_RESET
+            ? response()->json(['success' => true])
+            : response()->json(['error' => 'Failed to reset password'], 500);
     }
 }
